8118f162e18bca708a79d6dba11529a0aa903436
Fedor Indutny <fedor@indutny.com>
2017-06-21 23:42:18 -0400

    pm2: fix lint error

--- pm2.js
+++ pm2.js
@@ -4,5 +4,7 @@ const micro = require('micro');
 const app = require('./server');
 
 const server = micro(app).listen(process.env.NODE_PORT || 8000, () => {
+  /* eslint-disable no-console */
   console.log('Listening on %j', server.address());
+  /* eslint-enable no-console */
 });
